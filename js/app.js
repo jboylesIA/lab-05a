8ba969821ac1f9190311f3392e9e24480f23eb77
Josh Boyles <jboycr@yahoo.com>
2022-06-24 15:28:35 -0500

    answer to  problem 2

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 /**
  * 
  * @param {number} a 
- * @param {number*} b 
+ * @param {number} b 
  * @returns {Array} - sum of the numbers, string
  */
 function sum(a, b) { //eslint-disable-line
@@ -34,12 +34,19 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ */
 function multiply(a, b) { //eslint-disable-line
-
+    let result = a * b;
+    let resultsString = `The product of ${a} and ${b} is ${result}.`;
+    return [result, resultsString];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
