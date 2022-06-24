59cb4207eaf4a23dd6295a1acabe7e60d290ec45
Josh Boyles <jboycr@yahoo.com>
2022-06-24 14:30:37 -0500

    Question 1 answer.

--- js/app.js
+++ js/app.js
@@ -8,9 +8,12 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function addNumbers(a, b) { //eslint-disable-line
+console.log(a + b);
 
 }
+
+addNumbers(4,7);
 
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
