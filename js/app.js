4923ea587e1be5e9be9b05bb28a6abfccdf9f848
Josh Boyles <jboycr@yahoo.com>
2022-06-24 15:12:14 -0500

    completed problem 1

--- js/app.js
+++ js/app.js
@@ -8,15 +8,20 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function addNumbers(a, b) { //eslint-disable-line
-console.log(a + b);
-//test
+/**
+ * 
+ * @param {number} a 
+ * @param {number*} b 
+ * @returns {Array} - sum of the numbers, string
+ */
+function sum(a, b) { //eslint-disable-line
+    let result = a + b;
+    let resultsString = `The sum of ${a} and ${b} is ${result}.`; 
+    return [result, resultsString];
 }
 
-addNumbers(4,7);
-
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
